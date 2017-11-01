153ba2166331b2f2521cdfaad72bbba61f4d6f01
root <root@localhost.localdomain>
2017-11-01 12:09:41 +1100

    Rearranged

--- hello_world.c
+++ hello_world.c
@@ -3,9 +3,9 @@
 
 int
 main (int argc, char **argv)	{
-	printf ("\nTotal no. of arguments: %d",argc);
 	printf ("\nArgument-1: %d",*argv[0]);
 	printf ("\nArgument-2: %d",*argv[1]);
+	printf ("\nTotal no. of arguments: %d",argc);
 	printf ("\n\nWelcome to the world of Australia\n\n");
 	return 0;
 }
