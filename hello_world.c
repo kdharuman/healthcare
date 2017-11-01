a5413d51d10f1e37109035719088531f9cd100a2
root <root@localhost.localdomain>
2017-11-01 11:25:32 +1100

    Printed the first argument

--- hello_world.c
+++ hello_world.c
@@ -4,6 +4,7 @@
 int
 main (int argc, char **argv)	{
 	printf ("\nTotal no. of arguments: %d",argc);
+	printf ("\nArgument-1: %d",argv[0][0]);
 	printf ("\n\nWelcome to the world of Australia\n\n");
 	return 0;
 }
