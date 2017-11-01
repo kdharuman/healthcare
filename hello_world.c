c132b78265e12e5cb5c7202b9b64b24ab1bc4406
root <root@localhost.localdomain>
2017-11-01 12:45:49 +1100

    Added a string to hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -5,7 +5,7 @@ int
 main (int argc, char **argv)	{
 	printf ("\nArgument-1: %d",*argv[0]);
 	printf ("\nArgument-2: %d",*argv[1]);
-	printf ("\nTotal no. of arguments: %d",argc);
+	printf ("\nTotal no. of arguments passed: %d",argc);
 	printf ("\n\nWelcome to the world of Australia\n\n");
 	return 0;
 }
