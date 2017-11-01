db30610fd34dfa2e82dbf0f47d2ab04ea917a6f7
root <root@localhost.localdomain>
2017-11-01 11:36:23 +1100

    Removed the second array index

--- hello_world.c
+++ hello_world.c
@@ -4,8 +4,8 @@
 int
 main (int argc, char **argv)	{
 	printf ("\nTotal no. of arguments: %d",argc);
-	printf ("\nArgument-1: %d",argv[0][0]);
-	printf ("\nArgument-2: %d",argv[0][1]);
+	printf ("\nArgument-1: %d",*argv[0]);
+	printf ("\nArgument-2: %d",*argv[1]);
 	printf ("\n\nWelcome to the world of Australia\n\n");
 	return 0;
 }
