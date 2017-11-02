bab8e104a3863bf64f8374ce31f69f647d2b3b13
root <root@localhost.localdomain>
2017-11-02 12:55:55 +1100

    Modified 2 files

--- hello_world.c
+++ hello_world.c
@@ -7,5 +7,6 @@ main (int argc, char **argv)	{
 	printf ("\nArgument-2: %d",*argv[1]);
 	printf ("\nTotal no. of arguments passed: %d",argc);
 	printf ("\n\nWelcome to the world of Computer Programming\n\n");
+	printf ("\nWe do good programming");
 	return 0;
 }
