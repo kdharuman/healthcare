2aa44b4b30a4f1a5c9977e2e913247a0c4f80a8a
root <root@localhost.localdomain>
2017-11-08 12:44:47 +1100

    Modified

--- hello_world.c
+++ hello_world.c
@@ -7,6 +7,6 @@ main (int argc, char **argv)	{
 	printf ("\nArgument-2: %d",*argv[1]);
 	printf ("\nTotal no. of arguments passed: %d",argc);
 	printf ("\n\nWelcome to the world of Advanced Software Engineering\n\n");
-	printf ("\nWe do good programming in C");
+	printf ("\nWe do good programming in C & C++");
 	return 0;
 }
